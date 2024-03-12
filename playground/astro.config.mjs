9997fc2f020e974e0217b62ae921ef9ac21e3360
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-11 19:53:03 -0700

    Start

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
 import astroHashnode from "@matthiesenxyz/astro-hashnode";
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+	// output: 'server',
+	// adapter: node({
+	// 	mode: 'standalone',
+	// }),
 	integrations: [
 		astroHashnode({
 			hashnodeURL: "astroplayground.hashnode.dev",
